750cea1cea324067367526aa461f7acfd0ac4a96
André L. Abrantes <andredossantosabrantes@gmail.com>
2016-03-21 18:55:28 -0300

    fix FirebaseService

--- www/js/firebaseService.js
+++ www/js/firebaseService.js
@@ -1,16 +1,21 @@
 var povmt = angular.module('povmt');
 
-povmt.service("FirebaseService", ["$firebaseArray", "AuthService", 
+povmt.service("FirebaseService", ["$firebaseArray", "AuthService",
     function FirebaseService($firebaseArray, AuthService) {
         var self = this;
 
         var FIREBASE_URI = "https://povmt.firebaseio.com/";
 
         this.getArrayEntidades = function(nomeEntidade) {
-        	var userId = AuthService.getUsuarioLogado().uid;
+            var userId = AuthService.getUsuarioLogado().uid;
             var referencia = new Firebase(FIREBASE_URI + nomeEntidade + "/" + userId);
             return $firebaseArray(referencia);
         };
-	
+
+        this.getArraySubEntidades = function(nomeEntidade, nomeSubEntidade) {
+            var userId = AuthService.getUsuarioLogado().uid;
+            var referencia = new Firebase(FIREBASE_URI + nomeEntidade + "/" + userId + "/" + nomeSubEntidade);
+            return $firebaseArray(referencia);
+        };
     }
 ]);
