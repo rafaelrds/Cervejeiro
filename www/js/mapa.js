346aadf45d9fdbc915621e70f823aced373d4045
rafaelrds <crafards@gmail.com>
2016-07-28 12:33:28 -0300

    Add Mapa do Cervejeiro

--- www/js/mapa.js
+++ www/js/mapa.js
@@ -1,7 +1,7 @@
 angular.module('cervejeiro')
 
 .controller('MapaCtrl', function(
-    $scope, FirebaseService, $http, $ionicLoading) {
+    $scope, FirebaseService, $http, $ionicLoading, $state, $cordovaGeolocation) {
 
     var self = this;
 
@@ -11,5 +11,22 @@ angular.module('cervejeiro')
     $scope.$parent.setExpanded(true);
     $scope.$parent.setHeaderFab('right');
 
+    var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  }, function(error){
+    console.log("Could not get location");
+  });
     console.log("UH PAPAI CHEGOU");
 });
